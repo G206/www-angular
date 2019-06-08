b8c59302f01fe67fa6b0ba382132256e565c8f4d
Gabe <gabe@w3dev.io>
2019-06-08 15:03:08 -0700

    Modified package-lock due to alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'www-angular';
+  shouldShow = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AppMaterialModule} from './modules/app-material.module';
+import {AppAnimationModule} from './modules/app-animation.module';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './home-content/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule,
+    AppAnimationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
